c0764bb642bdb9682e322be595e3e31ca5fbfe94
Rene Sahmkow <rene.sahmkow@db-n.com>
2020-07-24 14:19:33 +0200

    post from  frontend to backend

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,26 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    vocabularys: {
+      list: []
+    }
+  },
+  mutations: {
+    setVocabularysList(state, value) {
+      state.vocabularys.list = value;
+    }
+  },
+  actions: {
+    async getData({ commit }) {
+      const data = await axios.get('http://localhost:5000/posts');
+
+      commit('setVocabularysList', data.data);
+    }
+  },
   modules: {}
 });
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="home">HomePage</div>
+  <section>
+    <div v-if="loading">Loading Animation</div>
+    <div v-else class="home">HomePage</div>
+  </section>
 </template>
 
 <script>
 export default {
   name: "Home",
-  components: {}
+  components: {},
+  data() {
+    return {
+      loading: false
+    };
+  },
+  computed: {},
+  async created() {
+    this.loading = true;
+
+    await this.$store.dispatch("getData");
+
+    this.loading = false;
+  },
+  mounted() {}
 };
 </script>
