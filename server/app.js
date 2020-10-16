bdbd88b5c842471cee282c57f3bf380af0fc476c
Rene Sahmkow <rene.sahmkow@db-n.com>
2020-10-16 10:19:37 +0200

    update cors

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 // cors
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Origin', 'https:vocabulina.netlify.app/');
+  res.header('Access-Control-Allow-Origin', 'https://vocabulina.netlify.app/');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
